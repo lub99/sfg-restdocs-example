cd0687e336beaf0510e5042e768183bc3bd8b016
lubwizard <matej.luburic@stack-wizard.com>
2022-12-02 16:41:37 +0100

    Add documenting validation constraints.

--- src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
+++ src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
@@ -49,6 +49,7 @@ public class BeerDto {
     @NotNull
     private BigDecimal price;
 
+    @Positive
     private Integer quantityOnHand;
 
 }
--- src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -12,10 +12,12 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -27,6 +29,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
@@ -81,6 +84,8 @@ class BeerControllerTest {
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
+        ConstrainedFields constrainedFields = new ConstrainedFields(BeerDto.class);
+
         mockMvc.perform(post("/api/v1/beer/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
@@ -89,15 +94,15 @@ class BeerControllerTest {
                         document(
                                 "v1/beer",
                                 requestFields(
-                                        fieldWithPath("id").ignored(),
-                                        fieldWithPath("version").ignored(),
-                                        fieldWithPath("createdDate").ignored(),
-                                        fieldWithPath("lastModifiedDate").ignored(),
-                                        fieldWithPath("beerName").description("Beer name "),
-                                        fieldWithPath("beerStyle").description("Beer style"),
-                                        fieldWithPath("upc").description("UPC of beer").attributes(),
-                                        fieldWithPath("price").description("Price"),
-                                        fieldWithPath("quantityOnHand").ignored()
+                                        constrainedFields.withPath("id").ignored(),
+                                        constrainedFields.withPath("version").ignored(),
+                                        constrainedFields.withPath("createdDate").ignored(),
+                                        constrainedFields.withPath("lastModifiedDate").ignored(),
+                                        constrainedFields.withPath("beerName").description("Beer name "),
+                                        constrainedFields.withPath("beerStyle").description("Beer style"),
+                                        constrainedFields.withPath("upc").description("UPC of beer").attributes(),
+                                        constrainedFields.withPath("price").description("Price"),
+                                        constrainedFields.withPath("quantityOnHand").ignored()
                                 )
                         )
                 );
@@ -122,6 +127,25 @@ class BeerControllerTest {
                 .upc(123123123123L)
                 .build();
 
+    }
+
+    private static class ConstrainedFields{
+
+        private final ConstraintDescriptions constraintDescriptions;
+
+        private ConstrainedFields(Class<?> classObj) {
+            this.constraintDescriptions = new ConstraintDescriptions(classObj);
+        }
+
+        private FieldDescriptor withPath(String path){
+            return fieldWithPath(path)
+                    .attributes(
+                            key("constraints").value(
+                                    StringUtils.collectionToDelimitedString(
+                                            constraintDescriptions.descriptionsForProperty(path), ". ")
+                            )
+                    );
+        }
     }
 
 }
